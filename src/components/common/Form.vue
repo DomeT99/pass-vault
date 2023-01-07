c150ebaed31b1864e53046756c22770b654d8514
Domenico Tenace <domenicotenace99@gmail.com>
2023-01-07 11:30:43 +0100

    feat: create form component

---
+++ src/components/common/Form.vue
@@ -0,0 +1,5 @@
+<template>
+    <form class="p-4">
+        <slot></slot>
+    </form>
+</template>
--- src/views/SignIn.vue
+++ src/views/SignIn.vue
@@ -1,8 +1,24 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import Form from '../components/common/Form.vue';
+</script>
 <template>
   <section class="bg-primary min-vh-100 v-align-center">
     <article class="row-cols-sm-1">
-      <div class="col-sm-12 justify-content-center d-flex"></div>
+      <div class="col-sm-12 justify-content-center d-flex">
+        <Form>
+          <div class="mb-3">
+            <label for="email" class="form-label text-white">Email</label>
+            <input type="email" class="form-control" id="email">
+          </div>
+          <div class="mb-4">
+            <label for="password" class="form-label text-white">Password</label>
+            <input type="password" class="form-control" id="password">
+          </div>
+          <div class="justify-content-center d-flex">
+            <button type="submit" class="btn btn-primary w-100">Submit</button>
+          </div>
+        </Form>
+      </div>
     </article>
   </section>
 </template>
@@ -12,5 +28,9 @@
   display: flex;
   align-items: center;
   justify-content: center;
+}
+
+.w-100 {
+  width: 100% !important;
 }
 </style>
