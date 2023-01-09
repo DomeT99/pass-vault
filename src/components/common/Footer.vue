841447ee61b54c00a135e2027e1fd5ba292248cb
Domenico Tenace <domenicotenace99@gmail.com>
2023-01-09 12:27:53 +0100

    feat: add footer

---
+++ src/components/common/Footer.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+import { computed } from "vue";
+
+let dateNow = computed(() => {
+  return new Date().getFullYear();
+});
+
+function openLinkTree() {
+  window.open("https://linktr.ee/domenicotenace");
+}
+</script>
+<template>
+  <footer class="d-flex justify-content-center py-3 my-4 border-top">
+    <p @click="openLinkTree" class="pointer">
+      &copy; {{ dateNow }} Domenico Tenace
+    </p>
+  </footer>
+</template>
+<style scoped>
+.pointer {
+  cursor: pointer;
+}
+</style>
--- src/layout/Default.vue
+++ src/layout/Default.vue
@@ -1,10 +1,12 @@
 <script setup lang="ts">
-import Navbar from '../components/common/Navbar.vue';
+import Navbar from "../components/common/Navbar.vue";
+import Footer from "../components/common/Footer.vue";
 </script>
 
 <template>
-    <Navbar />
-    <section class="min-vh-100">
-        <slot></slot>
-    </section>
+  <Navbar />
+  <section class="min-vh-100">
+    <slot></slot>
+  </section>
+  <Footer />
 </template>
