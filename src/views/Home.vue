c555e90bd68519a62744ff0f918aaaa93f89fd35
Domenico Tenace <domenicotenace99@gmail.com>
2023-01-10 17:57:56 +0100

    feat:add sticky button bottom right

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,6 +2,12 @@
 import Layout from "../layout/Default.vue";
 import Table from "../components/common/Table.vue";
 import Filter from "../components/Home/Filter.vue";
+import Button from "../components/common/Button.vue";
+import { computed } from "vue";
+
+const btnIcon = computed(() => {
+  return new URL("../assets/images/plus-solid.svg", import.meta.url).href;
+});
 </script>
 <template>
   <Layout>
@@ -11,10 +17,29 @@ import Filter from "../components/Home/Filter.vue";
     <section class="container pt-5">
       <Table />
     </section>
+
+    <section class="button-bar">
+      <Button
+        class="btn-primary btn-pad"
+        :prevent-default="true"
+        :type="'button'"
+        ><i :src="btnIcon"></i
+      ></Button>
+    </section>
   </Layout>
 </template>
-<style scoped>
+<style scoped lang="scss">
 .pt-10 {
   padding-top: 10rem;
+}
+.button-bar {
+  padding: 2rem;
+  position: fixed;
+  bottom: 0;
+  right: 0;
+}
+.btn-pad {
+  border-radius: 50%;
+  padding: 30px !important;
 }
 </style>
