478c5b1b6875837113707af5e5646e9d80697387
Domenico Tenace <domenicotenace99@gmail.com>
2023-01-18 20:29:52 +0100

    feat: create LyraSearch class

--- src/modules/lyra.ts
+++ src/modules/lyra.ts
@@ -1,12 +1,28 @@
-import { create } from "@lyrasearch/lyra";
+import { create, insert, search } from "@lyrasearch/lyra";
+import { PswDBModel } from "./models";
 
 export const pswDB = await create({
   schema: {
-    id: "number",
+    id: "string",
     site: "string",
     description: "string",
     username: "string",
     password: "string",
   },
 });
+
+export class LyraSearch {
+
+  public static async lyraInsert(params: PswDBModel) {
+    await insert(pswDB, params);
+  }
+
+  public static async lyraSearch(term: string) {
+    const searchResult = await search(pswDB, {
+      term: term,
+    });
+    return searchResult;
+  }
+}
+
 
---
+++ src/modules/models.ts
@@ -0,0 +1,7 @@
+export interface PswDBModel {
+    id: string;
+    site: string;
+    description: string;
+    username: string;
+    password: string;
+}
