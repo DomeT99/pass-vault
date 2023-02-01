5d27eb371e189f04c1fbf162bb588be8768379b3
Domenico Tenace <domenicotenace99@gmail.com>
2023-02-01 17:51:28 +0100

    feat: setup route to update password

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,27 +1,31 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHistory } from "vue-router";
 
 const routes = [
   {
     path: "/home",
     name: "sign-in",
-    component: () => import('../views/SignIn.vue')
+    component: () => import("../views/SignIn.vue"),
   },
   {
     path: "/",
     name: "home",
-    component: () => import('../views/Home.vue')
+    component: () => import("../views/Home.vue"),
   },
   {
     path: "/addpassword",
     name: "addpassword",
-    component: () => import('../views/AddPassword.vue')
-  }
-  
-]
+    component: () => import("../views/AddPassword.vue"),
+  },
+  {
+    path: "/updatepassword/:id",
+    name: "updatepassword",
+    component: () => import("../views/AddPassword.vue"),
+  },
+];
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: routes
-})
+  routes: routes,
+});
 
-export default router
+export default router;
