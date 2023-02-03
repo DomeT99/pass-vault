c3a97a6e1baa8b503e959e9251bcc625d28e9b03
Domenico <domenicotenace99@gmail.com>
2023-02-03 20:02:40 +0100

    feat: dev updatePassword logic

--- src/store/passwordStore.ts
+++ src/store/passwordStore.ts
@@ -5,46 +5,57 @@ import { PswDBModel } from "../modules/models";
 
 //Firebase modules
 import { db } from "../firebase/firebase";
-import { collection, getDocs, addDoc } from "firebase/firestore";
+import { collection, getDocs, addDoc, doc, getDoc } from "firebase/firestore";
 
 //Utils
 import { Utils } from "../utils/utils";
 import router from "../router";
 
 export const usePasswordStore = defineStore("passwordStore", () => {
-  /**ACTIONS */
-  async function populateTable(dbData: PswDBModel[]): Promise<PswDBModel[]> {
-    try {
-      const querySnapshot = await getDocs(collection(db, "Container-Pass"));
+	/**ACTIONS */
+	async function populateTable(dbData: PswDBModel[]): Promise<PswDBModel[]> {
+		try {
+			const querySnapshot = await getDocs(collection(db, "Container-Pass"));
 
-      querySnapshot.forEach((doc) => {
-        let dataFirebase = {
-          id: doc.data().id,
-          description: doc.data().description,
-          site: doc.data().site,
-          username: doc.data().username,
-          password: doc.data().password,
-        } satisfies PswDBModel;
+			querySnapshot.forEach((doc) => {
+				let dataFirebase = {
+					id: doc.data().id,
+					description: doc.data().description,
+					site: doc.data().site,
+					username: doc.data().username,
+					password: doc.data().password,
+				} satisfies PswDBModel;
 
-        dbData.push(dataFirebase);
-      });
+        console.log(doc.data());
+				dbData.push(dataFirebase);
+			});
+			return dbData;
+		} catch (e) {
+			throw e;
+		}
+	}
 
-      return dbData;
-    } catch (e) {
-      throw e;
-    }
-  }
+	async function addNewPassword(newPassword: PswDBModel) {
+		try {
+			if (Utils.checkForm(newPassword)) {
+				await addDoc(collection(db, "Container-Pass"), newPassword);
+				router.push("/");
+			}
+		} catch (e) {
+			throw e;
+		}
+	}
 
-  async function addNewPassword(newPassword: PswDBModel) {
-    try {
-      if (Utils.checkForm(newPassword)) {
-        await addDoc(collection(db, "Container-Pass"), newPassword);
-        router.push("/");
-      }
-    } catch (e) {
-      throw e;
-    }
-  }
+	async function getDocument(id: string): Promise<PswDBModel> {
+		const docRef = doc(db, "Container-Pass", id);
+		const docSnap = await getDoc(docRef);
 
-  return { populateTable, addNewPassword };
+		if (docSnap.exists()) {
+			return docSnap.data() as PswDBModel;
+		} else {
+			throw new Error("The document doesn't exist.");
+		}
+	}
+
+	return { populateTable, addNewPassword, getDocument };
 });
--- src/views/AddPassword.vue
+++ src/views/AddPassword.vue
@@ -1,162 +1,190 @@
 <script setup lang="ts">
-//Vue native modules
-import { defineAsyncComponent, computed, reactive } from "vue";
+	//Vue native modules
+	import {
+		defineAsyncComponent,
+		computed,
+		reactive,
+		onMounted,
+		watch,
+	} from "vue";
 
-//Components
-import Layout from "../layout/Default.vue";
-import Input from "../components/common/Input.vue";
-import Loading from "../components/common/Loading.vue";
-import TextArea from "../components/common/Textarea.vue";
-import Button from "../components/common/Button.vue";
-import router from "../router";
+	//Components
+	import Layout from "../layout/Default.vue";
+	import Input from "../components/common/Input.vue";
+	import Loading from "../components/common/Loading.vue";
+	import TextArea from "../components/common/Textarea.vue";
+	import Button from "../components/common/Button.vue";
 
-//Modules
-import { PswDBModel } from "../modules/models";
+	//Router
+	import router from "../router";
+	import { useRoute } from "vue-router";
 
-//Store
-import { usePasswordStore } from "../store/passwordStore";
-const pswStore = usePasswordStore();
+	//Modules
+	import { PswDBModel } from "../modules/models";
 
-const Form = defineAsyncComponent({
-  loader: () => import("../components/common/Form.vue"),
-  loadingComponent: Loading,
-  delay: 700,
-  timeout: 5000,
-});
+	//Store
+	import { usePasswordStore } from "../store/passwordStore";
+	const pswStore = usePasswordStore();
 
-let newPassword: PswDBModel = reactive({
-  id: "",
-  username: "",
-  site: "",
-  description: "",
-  password: "",
-});
+  //Async Form component
+	const Form = defineAsyncComponent({
+		loader: () => import("../components/common/Form.vue"),
+		loadingComponent: Loading,
+		delay: 700,
+		timeout: 5000,
+	});
 
-//Computed Props
-const btnIcon = computed(() => {
-  return new URL("../assets/images/arrow-left-solid.svg", import.meta.url).href;
-});
+  //Istance of router
+	const route = useRoute();
 
-function returnHomePage() {
-  router.push("/");
-}
+	let password: PswDBModel = reactive({
+		id: "",
+		username: "",
+		site: "",
+		description: "",
+		password: "",
+	});
 
-function resetForm() {
-  newPassword.id = "";
-  newPassword.username = "";
-  newPassword.site = "";
-  newPassword.description = "";
-  newPassword.password = "";
-}
+	//Computed Props
+	const btnIcon = computed(() => {
+		return new URL("../assets/images/arrow-left-solid.svg", import.meta.url)
+			.href;
+	});
+
+	function returnHomePage() {
+		router.push("/");
+	}
+
+	function resetForm() {
+		password.id = "";
+		password.username = "";
+		password.site = "";
+		password.description = "";
+		password.password = "";
+	}
+
+	onMounted(async () => {
+		if (route.params.id !== null && typeof route.params.id === "string") {
+			let document = await pswStore.getDocument(route.params.id);
+
+			if (document) {
+				password.id = document.id;
+				password.username = document.username;
+				password.site = document.site;
+				password.description = document.description;
+				password.password = document.password;
+			}
+		}
+	});
 </script>
 
 <template>
-  <Layout>
-    <Form class="pt-10 v-align-center min-vh-40 fade-in">
-      <section class="container">
-        <div class="row d-flex justify-content-center g-4">
-          <div class="col-sm-3 col-lg-1">
-            <Input
-              :value="newPassword.id"
-              :place-holder="'Id'"
-              class="form-control form-control-lg addpsw"
-              v-model="newPassword.id"
-            />
-          </div>
-          <div class="col-sm-5 col-lg-3">
-            <Input
-              :value="newPassword.username"
-              :place-holder="'Username/Email'"
-              class="form-control form-control-lg addpsw"
-              v-model="newPassword.username"
-            />
-          </div>
-          <div class="col-sm-4 col-lg-4">
-            <Input
-              :value="newPassword.site"
-              :place-holder="'Site/Application'"
-              class="form-control form-control-lg addpsw"
-              v-model="newPassword.site"
-            />
-          </div>
-        </div>
-        <div class="row d-flex justify-content-center g-4 pt-5">
-          <div class="col-sm-12 col-lg-8 col-md-12">
-            <TextArea
-              :value="newPassword.description"
-              :rows="7"
-              :resize="false"
-              :place-holder="'Description'"
-              class="form-control form-control-lg addpsw"
-              v-model="newPassword.description"
-            />
-          </div>
-        </div>
+	<Layout>
+		<Form class="pt-10 v-align-center min-vh-40 fade-in">
+			<section class="container">
+				<div class="row d-flex justify-content-center g-4">
+					<div class="col-sm-3 col-lg-1">
+						<Input
+							:value="password.id"
+							:place-holder="'Id'"
+							class="form-control form-control-lg addpsw"
+							v-model="password.id"
+						/>
+					</div>
+					<div class="col-sm-5 col-lg-3">
+						<Input
+							:value="password.username"
+							:place-holder="'Username/Email'"
+							class="form-control form-control-lg addpsw"
+							v-model="password.username"
+						/>
+					</div>
+					<div class="col-sm-4 col-lg-4">
+						<Input
+							:value="password.site"
+							:place-holder="'Site/Application'"
+							class="form-control form-control-lg addpsw"
+							v-model="password.site"
+						/>
+					</div>
+				</div>
+				<div class="row d-flex justify-content-center g-4 pt-5">
+					<div class="col-sm-12 col-lg-8 col-md-12">
+						<TextArea
+							:value="password.description"
+							:rows="7"
+							:resize="false"
+							:place-holder="'Description'"
+							class="form-control form-control-lg addpsw"
+							v-model="password.description"
+						/>
+					</div>
+				</div>
 
-        <div class="row d-flex justify-content-center g-4 pt-5">
-          <div class="col-sm-12 col-lg-8 col-md-12">
-            <Input
-              :value="newPassword.password"
-              :type="'password'"
-              :place-holder="'Password'"
-              class="form-control form-control-lg psw"
-              v-model="newPassword.password"
-            />
-          </div>
-        </div>
+				<div class="row d-flex justify-content-center g-4 pt-5">
+					<div class="col-sm-12 col-lg-8 col-md-12">
+						<Input
+							:value="password.password"
+							:type="'password'"
+							:place-holder="'Password'"
+							class="form-control form-control-lg psw"
+							v-model="password.password"
+						/>
+					</div>
+				</div>
 
-        <div class="row d-flex justify-content-center g-4 pt-5">
-          <div class="col-sm-12 col-lg-4 col-md-6">
-            <Button
-              :fn-button="resetForm"
-              class="btn-fifth w-100"
-              :type="'submit'"
-              >Reset</Button
-            >
-          </div>
-          <div class="col-sm-12 col-lg-4 col-md-6">
-            <Button
-              class="btn-primary w-100"
-              :type="'submit'"
-              :fn-button="() => pswStore.addNewPassword(newPassword)"
-              >Save</Button
-            >
-          </div>
-        </div>
-      </section>
-    </Form>
-    <section class="button-bar">
-      <Button
-        :fn-button="returnHomePage"
-        class="btn-secondary btn-pad"
-        :type="'button'"
-        ><img :src="btnIcon" width="25" height="25"
-      /></Button>
-    </section>
-  </Layout>
+				<div class="row d-flex justify-content-center g-4 pt-5">
+					<div class="col-sm-12 col-lg-4 col-md-6">
+						<Button
+							:fn-button="resetForm"
+							class="btn-fifth w-100"
+							:type="'submit'"
+							>Reset</Button
+						>
+					</div>
+					<div class="col-sm-12 col-lg-4 col-md-6">
+						<Button
+							class="btn-primary w-100"
+							:type="'submit'"
+							:fn-button="() => pswStore.addNewPassword(password)"
+							>Save</Button
+						>
+					</div>
+				</div>
+			</section>
+		</Form>
+		<section class="button-bar">
+			<Button
+				:fn-button="returnHomePage"
+				class="btn-secondary btn-pad"
+				:type="'button'"
+				><img :src="btnIcon" width="25" height="25"
+			/></Button>
+		</section>
+	</Layout>
 </template>
 <style scoped>
-/**Custom style for this page */
-.v-align-center {
-  display: flex;
-  align-items: center;
-  justify-content: center;
-}
-.button-bar {
-  padding: 2rem;
-  position: fixed;
-  bottom: 0;
-  left: 0;
-}
-.btn-pad {
-  border-radius: 50%;
-  padding: 10px !important;
-}
-.min-vh-40 {
-  min-height: 40vh;
-}
+	/**Custom style for this page */
+	.v-align-center {
+		display: flex;
+		align-items: center;
+		justify-content: center;
+	}
+	.button-bar {
+		padding: 2rem;
+		position: fixed;
+		bottom: 0;
+		left: 0;
+	}
+	.btn-pad {
+		border-radius: 50%;
+		padding: 10px !important;
+	}
+	.min-vh-40 {
+		min-height: 40vh;
+	}
 
-.pt-10 {
-  padding-top: 6rem !important;
-}
+	.pt-10 {
+		padding-top: 6rem !important;
+	}
 </style>
