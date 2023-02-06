a9c01446d6de7ebfca27e37ee49d38cf8dd36839
Domenico <domenicotenace99@gmail.com>
2023-02-06 18:52:48 +0100

    change: logic about table and pagination

--- src/components/Home/SectionTable.vue
+++ src/components/Home/SectionTable.vue
@@ -1,56 +1,45 @@
 <script setup lang="ts">
-/**Section that is loaded asynchronously in Home views*/
-import { onMounted, onUnmounted, onUpdated, ref, reactive } from "vue";
+	/**Section that is loaded asynchronously in Home views*/
+	import { onMounted, ref, reactive } from "vue";
 
-//Components
-import Table from "./Table.vue";
-import Filter from "./Filter.vue";
-import Pagination from "./Pagination.vue";
+	//Components
+	import Table from "./Table.vue";
+	import Filter from "./Filter.vue";
 
-//Modules
-import { PswDBModel } from "../../modules/models";
+	//Modules
+	import { PswDBModel } from "../../modules/models";
 
-//Third Part Library
-import _ from "lodash";
+	//Third Part Library
+	import _ from "lodash";
 
-//Store
-import { usePasswordStore } from "../../store/passwordStore";
+	//Store
+	import { usePasswordStore } from "../../store/passwordStore";
 
-const pswStore = usePasswordStore();
+	const pswStore = usePasswordStore();
 
-let idTable: string = _.uniqueId("");
-let showPagination = ref(false);
-let dbData: PswDBModel[] = reactive([]);
+	let idTable: string = _.uniqueId("");
+	let showPagination = ref(false);
+	let dbData: PswDBModel[] = reactive([]);
 
-onMounted(async () => {
-  try {
-    dbData = await pswStore.populateTable(dbData);
-    if (dbData.length !== 0) {
-      showPagination.value = true;
-    }
-  } catch (e) {
-    throw e;
-  }
-});
-
+	onMounted(async () => {
+		try {
+			dbData = await pswStore.populateTable(dbData);
+		} catch (e) {
+			throw e;
+		}
+	});
 </script>
 
 <template>
-  <section class="container pt-10 fade-in">
-    <Filter />
-  </section>
-  <section class="container pt-5 fade-in table-responsive">
-    <Table :id="idTable" />
-  </section>
-
-  <template v-if="showPagination">
-    <section class="d-flex justify-content-center container pt-5 fade-in">
-      <Pagination :id="idTable" :data-json="dbData" />
-    </section>
-  </template>
+	<section class="container pt-10 fade-in">
+		<Filter />
+	</section>
+	<section class="container pt-5 fade-in table-responsive">
+		<Table :id="idTable" :data-json="dbData" />
+	</section>
 </template>
 <style scoped>
-.pt-10 {
-  padding-top: 10rem;
-}
+	.pt-10 {
+		padding-top: 10rem;
+	}
 </style>
--- src/components/Home/Table.vue
+++ src/components/Home/Table.vue
@@ -1,23 +1,47 @@
 <script setup lang="ts">
-//Interface to describe the component's props
-interface TableModel {
-  id: string;
-}
-const props = defineProps<TableModel>();
+	import { DataJsonType } from "../../modules/types";
+
+	//Interface to describe the component's props
+	interface TableModel {
+		id: string;
+		dataJson: DataJsonType[];
+	}
+	const props = defineProps<TableModel>();
 </script>
 <template>
-  <table class="table table-bordered table-hover">
-    <thead class="table-secondary">
-      <tr>
-        <th scope="col">ID</th>
-        <th scope="col">Site/App</th>
-        <th scope="col">Username</th>
-        <th scope="col">Password</th>
-        <th scope="col"></th>
-        <th scope="col"></th>
-        <th scope="col"></th>
-      </tr>
-    </thead>
-    <tbody :id="props.id"></tbody>
-  </table>
+	<div class="h-40 overflow-y-scroll">
+		<table class="table table-bordered table-hover" :id="props.id">
+			<thead class="table-secondary">
+				<tr>
+					<th scope="col">ID</th>
+					<th scope="col">Site/App</th>
+					<th scope="col">Username</th>
+					<th scope="col">Password</th>
+					<th scope="col"></th>
+					<th scope="col"></th>
+					<th scope="col"></th>
+				</tr>
+			</thead>
+			<tbody>
+				<tr v-for="data in props.dataJson">
+					<td>{{ data.id }}</td>
+					<td>{{ data.site }}</td>
+					<td>{{ data.username }}</td>
+					<td>{{ data.password }}</td>
+					<td><button class="btn btn-fourth w-100">✒️</button></td>
+					<td><button class="btn btn-primary w-100">🔍</button></td>
+					<td><button class="btn btn-fifth w-100">❌</button></td>
+				</tr>
+			</tbody>
+		</table>
+	</div>
 </template>
+
+<style scoped>
+	.h-40 {
+		height: 40vh;
+	}
+	.overflow-y-scroll {
+		overflow-y: scroll;
+	}
+</style>
